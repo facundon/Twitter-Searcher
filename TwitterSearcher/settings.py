a4c6d320f2a3a5b727b70b2d01e83f8c491a7452
Facundo <facundonrodriguez@gmail.com>
2020-12-10 09:59:54 -0300

    se edito gitignore

--- TwitterSearcher/settings.py
+++ TwitterSearcher/settings.py
@@ -102,9 +102,9 @@ WSGI_APPLICATION = 'TwitterSearcher.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'EjercicioMaur',
-        'USER': 'Facu',
-        'PASSWORD': 'prueba',
+        'NAME': 'DB_NAME',
+        'USER': 'DB_USER',
+        'PASSWORD': 'DB_PASSWORD',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
